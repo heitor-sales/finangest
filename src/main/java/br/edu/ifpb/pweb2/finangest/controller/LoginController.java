21ef16a0fda9c4ef2a92124f828454d2473e633a
Lucasandre23 <lucasandre152015@gmail.com>
2025-07-11 10:56:45 -0300

    configuracoes iniciais de login

---
+++ src/main/java/br/edu/ifpb/pweb2/finangest/controller/LoginController.java
@@ -0,0 +1,29 @@
+package br.edu.ifpb.pweb2.finangest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    @RequestMapping("/form")
+    public String showLoginForm() {
+        return "form-login";
+    }
+    
+    @RequestMapping("/valide")
+    public String valideLogin(String login, String senha, Model model) {
+        if(login.equals("admin") && senha.equals("secret")){
+            model.addAttribute("login", login);
+            return "index";
+        } else {
+            model.addAttribute("erro","login inválido");
+            return "form-login";
+        }
+      
+    }
+    
+}
